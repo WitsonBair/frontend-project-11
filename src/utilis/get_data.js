eb210a98dd77877cc1f40d391c6c8ce2523f13dd
Bayar Khyolgo <tsyden2.bair@gmail.com>
2023-10-17 23:54:15 +0800

    Step 7.0.6 | Added disableCache for AllOrigin

--- src/utilis/get_data.js
+++ src/utilis/get_data.js
@@ -1,5 +1,5 @@
 export default async (url) => {
-  const rssData = fetch(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
+  const rssData = fetch(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
     .then((response) => {
       if (response.ok) return response.json();
       throw new Error('Network response was not ok.');
